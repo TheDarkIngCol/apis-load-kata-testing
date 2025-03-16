fd761b9d20f4000d1c2e122ab5e7c9c2b2ebe8e3
JTORR70 <JTORR70@bancodebogota.com.co>
2025-03-16 10:21:06 -0500

    Proyecto de carga actualizado rama qa

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # apis-load-kata-testing
+
 Proyecto de automatización de APIS con pruebas de carga con gatling para la presentación de la KATA - BDB
+
+**Comandos:**
+- Para ejecutar los test: ./mvnw gatling:test
+- Para instalar dependencias: ./mvnw install
+- Para limpiar el proyecto: ./mvnw clean
---
+++ src/test/scala/apiKata/postProducts/PostProductsSuccess.scala
@@ -0,0 +1,39 @@
+package apiKata.postProducts
+
+import io.gatling.core.Predef._
+import config.MethodsHttp.post
+import utilities.{BuildRequest, EnvironmentValues, RequestParams}
+
+class PostProductsSuccess extends Simulation {
+
+  val env = new EnvironmentValues()
+  val params = new RequestParams()
+
+  params.baseUrl = "https://fakestoreapi.com"
+  params.pathUrl = "/products"
+
+  val jsonPath = "data/datapost.json"
+
+  params.headers = Map(
+    "Content-Type" -> "application/json"
+  )
+
+  params.templateJson = jsonPath
+  params.method = post
+  params.requestName = "post model"
+  params.statusExpected = 200
+  params.scenarioName = "Envío del producto exitosamente"
+
+  val outputPath = "src/test/scala/apiKata/postProducts"
+  val request = new BuildRequest(params, outputPath)
+
+  val scenario1 = scenario("Modelo Abierto | Usuarios constantes por segundo").exec(request.scn).inject(constantUsersPerSec(3) during(2))
+  val scenario2 = scenario("Modelo Abierto | Usuarios fijos al mismo tiempo").exec(request.scn).inject(atOnceUsers(3))
+  val scenario3 = scenario("Modelo Abierto | Disminucion de usuarios").exec(request.scn).inject(rampUsersPerSec(5) to (2) during(10))
+  val scenario4 = scenario("Modelo Cerrado | Usuarios concurrentes por segundo").exec(request.scn).inject(constantConcurrentUsers(5) during(3))
+  val scenario5 = scenario("Modelo Cerrado | Aumento de usuarios concurrentes").exec(request.scn).inject(rampConcurrentUsers(3) to (6) during(5))
+
+  setUp(
+    scenario1, scenario2, scenario3, scenario4, scenario5
+  ).protocols(request.httpProtocol)
+}
