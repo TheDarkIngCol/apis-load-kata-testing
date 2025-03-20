986458b67ddefce8822976966c4acbeb579de1f0
JTORR70 <JTORR70@bancodebogota.com.co>
2025-03-19 20:35:57 -0500

    Actualizado .gitignore y eliminados archivos basura

--- src/test/scala/apiKata/putProducts/PutProductsSuccess.scala
+++ src/test/scala/apiKata/putProducts/PutProductsSuccess.scala
@@ -29,11 +29,11 @@ class PutProductsSuccess extends Simulation{
   val outputPath = "src/test/scala/apiKata/putProducts"
   val request = new BuildRequest(params, outputPath)
 
-  val scenario1 = scenario("Modelo Abierto | Usuarios constantes por segundo").exec(request.scn).inject(constantUsersPerSec(3) during(2))
+  val scenario1 = scenario("Modelo Abierto | Usuarios constantes por segundo").exec(request.scn).inject(constantUsersPerSec(4) during(2))
   val scenario2 = scenario("Modelo Abierto | Usuarios fijos al mismo tiempo").exec(request.scn).inject(atOnceUsers(3))
-  val scenario3 = scenario("Modelo Abierto | Disminucion de usuarios").exec(request.scn).inject(rampUsersPerSec(5) to (2) during(10))
-  val scenario4 = scenario("Modelo Cerrado | Usuarios concurrentes por segundo").exec(request.scn).inject(constantConcurrentUsers(5) during(3))
-  val scenario5 = scenario("Modelo Cerrado | Aumento de usuarios concurrentes").exec(request.scn).inject(rampConcurrentUsers(3) to (6) during(5))
+  val scenario3 = scenario("Modelo Abierto | Disminucion de usuarios").exec(request.scn).inject(rampUsersPerSec(7) to (2) during(2))
+  val scenario4 = scenario("Modelo Cerrado | Usuarios concurrentes por segundo").exec(request.scn).inject(constantConcurrentUsers(3) during(3))
+  val scenario5 = scenario("Modelo Cerrado | Aumento de usuarios concurrentes").exec(request.scn).inject(rampConcurrentUsers(1) to (5) during(5))
 
   setUp(
     scenario1, scenario2, scenario3, scenario4, scenario5
